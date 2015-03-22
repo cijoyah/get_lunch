4244e94fd64508e484094a2bd61b677394ea021c
Chris Ijoyah <chris@ijoyah.com>
2015-03-22 23:07:58 +0000

    created edit, destroy functions and views for promotions, and amended typos in controller

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,9 +1,12 @@
 class PromotionsController < ApplicationController
 
+    before_action :find_promotion, only: [:show, :edit, :update, :destroy]
+
     def index
+      @promotions = Promotion.all.order("created_at DESC")
     end
+
     def show
-      @promotion = Promotion.find(params[:id])
     end
 
     def create
@@ -23,12 +26,23 @@ class PromotionsController < ApplicationController
     end
 
     def update
+      if @promotion.update(promotion_params)
+        redirect_to @promotion
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+      @promotion.destroy
+      redirect_to root_path
     end
 
     private
+
+    def find_promotion
+      @promotion = Promotion.find(params[:id])
+    end
 
     def promotion_params
       params.require(:promotion).permit(:pitch, :description, :price, :available_on_date, :start_time, :end_time, :cuisine, :hotness)
