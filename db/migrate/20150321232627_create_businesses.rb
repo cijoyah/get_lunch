e00d64f0bee3ebd1633048e2969f44db2f7e1be5
Chris Ijoyah <chris@ijoyah.com>
2015-03-22 00:08:22 +0000

    create business and user model

---
+++ db/migrate/20150321232627_create_businesses.rb
@@ -0,0 +1,17 @@
+class CreateBusinesses < ActiveRecord::Migration
+  def change
+    create_table :businesses do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :postcode
+      t.string :website
+      t.string :tel
+      t.string :email
+      t.string :facebook
+      t.string :twitter
+
+      t.timestamps null: false
+    end
+  end
+end
