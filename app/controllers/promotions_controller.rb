df0bac422675fa1c5c3d82904f7db1396da8c28d
Chris Ijoyah <chris@ijoyah.com>
2015-03-22 00:56:33 +0000

    changed promotion index view to .haml and created crud functions in promo controller

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,5 +2,22 @@ class PromotionsController < ApplicationController
 
     def index
     end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
      
 end
