7975bd403185e0a82b86d83b2fcfdec676fb1430
Chris Ijoyah <chris@ijoyah.com>
2015-03-23 11:18:28 +0000

    added addition user info fields onto new/edit registrations devise views

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,27 @@
 class BusinessesController < ApplicationController
+
+  def index
+    @business = Business.all.
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
 end
