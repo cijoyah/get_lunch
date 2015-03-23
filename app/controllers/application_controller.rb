75e9af6b48179b98f48581521372471e2ead81f4
Chris Ijoyah <chris@ijoyah.com>
2015-03-23 18:43:01 +0000

    remote image uploader working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :name, :avatar, :address, :postcode, :city, :tel, :website, :facebook, :twitter) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :name, :avatar, :address, :postcode, :city, :tel, :website, :facebook, :twitter) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :name, :avatar, :address, :postcode, :city, :tel, :website, :facebook, :twitter, :remote_avatar_url) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :name, :avatar, :address, :postcode, :city, :tel, :website, :facebook, :twitter, :remote_avatar_url) }
   end
 
 end
