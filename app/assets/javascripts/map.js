ac60bc75ad07ca89afd8b3a34faf200b65eedbb1
Chris Ijoyah <chris@ijoyah.com>
2015-03-24 18:47:09 +0000

    google maps working and added to promotion show page, can now plot locations of user and promo

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,74 @@
+$(function(){
+
+var geocoder;
+var map;
+
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 17,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    codeAddress()
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
+
+        // var infowindow = new google.maps.InfoWindow({
+        //   map: map,
+        //   position: pos,
+        //   content: 'Location found using HTML5.'
+        // });
+        var marker = new google.maps.Marker({
+            map: map,
+            position: pos
+        });
+
+        // map.setCenter(pos);
+      }, function() {
+        handleNoGeolocation(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleNoGeolocation(false);
+    }
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: map,
+      position: new google.maps.LatLng(60, 105),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+  }
+
+  function codeAddress() {
+    var address = $('#address').text();
+    console.log(address)
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+google.maps.event.addDomListener(window, 'load', initialize);
+})
