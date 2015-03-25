f6dce3183f393f0ad5a2dea2508cd9063bbfd968
Chris Ijoyah <chris@ijoyah.com>
2015-03-25 09:39:46 +0000

    directions now working on maps :-)

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,74 +1,88 @@
 $(function(){
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 var geocoder;
 var map;
+// var startPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+var endPoint = $('#address').text();
 
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var mapOptions = {
-      zoom: 17,
-      center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    codeAddress()
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
 
-        // var infowindow = new google.maps.InfoWindow({
-        //   map: map,
-        //   position: pos,
-        //   content: 'Location found using HTML5.'
-        // });
-        var marker = new google.maps.Marker({
-            map: map,
-            position: pos
-        });
-
-        // map.setCenter(pos);
-      }, function() {
-        handleNoGeolocation(true);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleNoGeolocation(false);
-    }
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 17,
+    center: latlng
   }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  codeAddress();
+  directionsDisplay.setMap(map);
 
-  function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-      var content = 'Error: The Geolocation service failed.';
-    } else {
-      var content = 'Error: Your browser doesn\'t support geolocation.';
-    }
-
-    var options = {
-      map: map,
-      position: new google.maps.LatLng(60, 105),
-      content: content
-    };
-
-    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);
-  }
-
-  function codeAddress() {
-    var address = $('#address').text();
-    console.log(address)
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      // var marker = new google.maps.Marker({
+      //     map: map,
+      //     position: pos
+      // });
+      calcRoute(pos)
+      // map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
     });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
   }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+function codeAddress() {
+  var address = $('#address').text();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      // var marker = new google.maps.Marker({
+      //     map: map,
+      //     position: results[0].geometry.location
+      // });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function calcRoute(pos) {
+  // var selectedMode = document.getElementById('mode').value;
+  var request = {
+      origin: pos,
+      destination: endPoint,
+      travelMode: google.maps.TravelMode["WALKING"]
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
 })
