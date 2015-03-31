ec3236e99e0365f46ffd19687491e169dd291e69
Chris Ijoyah <chris@ijoyah.com>
2015-03-31 16:05:25 +0100

    added aws to secrets.yml again but this time correcting possible CAP mistake again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider  => 'AWS',  # required
-    :aws_access_key_id  => ENV['AWS_ACCESS_KEY_ID'],  # required
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],  # required
+    :AWS_ACCESS_KEY_ID => ENV["AWS_ACCESS_KEY_ID"],
+    :AWS_SECRET_ACCESS_KEY => ENV["AWS_SECRET_ACCESS_KEY"],
     :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET']  # required
